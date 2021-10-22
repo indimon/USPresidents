d5df97aefdaec004e077d43f8eba127babcec914
indimon <90929596+indimon@users.noreply.github.com>
2021-10-22 13:24:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pytest
 import requests
 
 url_ddg = "https://api.duckduckgo.com/?q=presidents+of+the+united+states&format=json"
-
 presidents = [" Washington", " Adams", " Jefferson", " Madison", " Monroe", " Adams", " Jackson", " Buren",
               " Harrison", " Tyler", " Polk", " Taylor", " Fillmore", " Pierce", " Buchanan", " Lincoln",
               " Johnson", " Grant", " Hayes", " Garfield", " Arthur", " Cleveland", " Harrison", " Cleveland",
@@ -14,5 +13,5 @@ presidents = [" Washington", " Adams", " Jefferson", " Madison", " Monroe", " Ad
 def test_ddg0():
     resp = requests.get(url_ddg + "/?q=DuckDuckGo&format=json")
     rsp_data = resp.json()
-    for x in presidents:
+    for x in presidents: 
         assert x in str(rsp_data)
